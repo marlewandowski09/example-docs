713b12e29e520f457b30742994e8d0d9a1408ee2
mrf <marlewandowski09@gmail.com>
2025-08-08 15:15:45 +0200

    new logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,10 +81,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'StreamData Docs',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'StreamData',
+          src: 'logo.svg',
         },
         items: [
           {
