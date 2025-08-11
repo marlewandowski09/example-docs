b9302c22fd28e451aecbe6224e36a8dd8c87e9e6
mrf <marlewandowski09@gmail.com>
2025-08-11 19:21:17 +0200

    new dashboard section

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import './carousel_css.css';
 
 export default function MyCarousel() {
   const settings = {
@@ -13,16 +14,18 @@ export default function MyCarousel() {
   };
 
   return (
+    <div style={{ width: '500px', margin: '0 auto' }}>
     <Slider {...settings}>
       <div>
-        <img src="/img/example-docs/graph-1.png" alt="Image 1" />
+        <img src="/example-docs/graph-1.png" alt="Image 1" style={{ width: '500px', display: 'block' }}/>
       </div>
       <div>
-        <img src="/img/example-docs/graph-2.png" alt="Image 2" />
+        <img src="/example-docs/graph-2.png" alt="Image 2" style={{ width: '500px', display: 'block' }}/>
       </div>
       <div>
-        <img src="/img/example-docs/graph-3.png" alt="Image 3" />
+        <img src="/example-docs/graph-3.png" alt="Image 3" style={{ width: '500px', display: 'block' }}/>
       </div>
     </Slider>
+    </div>
   );
 }
